f00ff9ab3d58db183266031cfbdaa290dade44b3
Derek <derekcoder@gmail.com>
2018-07-26 12:02:04 +0800

    Init

---
+++ CMarkKit/CMarkKit.h
@@ -0,0 +1,19 @@
+//
+//  CMarkKit.h
+//  CMarkKit
+//
+//  Created by Derek on 26/7/18.
+//  Copyright © 2018 Derek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CMarkKit.
+FOUNDATION_EXPORT double CMarkKitVersionNumber;
+
+//! Project version string for CMarkKit.
+FOUNDATION_EXPORT const unsigned char CMarkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMarkKit/PublicHeader.h>
+
+
